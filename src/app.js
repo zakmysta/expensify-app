9bdd4e885a3f6e1c2195f0bb170028fe65b915a2
Zaki Khan <zakmysta@gmail.com>
2017-10-23 22:53:52 -0400

    Fetch expenses data from firebase on load

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { startSetExpenses } from './actions/expenses';
 import './firebase/firebase';
 
 import 'normalize.css/normalize.css';
@@ -21,4 +20,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
