193ad60295db387f4541cb9ec0e71f74fcad6289
Zaki Khan <zakmysta@gmail.com>
2017-10-27 17:43:08 -0400

    Add Expensify styles

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import getVisibleExpenses from './selectors/expenses';
 import { login, logout } from './actions/auth';
 import { startSetExpenses } from './actions/expenses';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -27,7 +28,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
